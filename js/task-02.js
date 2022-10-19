1811072b3729b1957bce048053f4c3e22a44c23d
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-19 17:23:36 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const makeListItems = items => {
+  return items.map(item => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('item');
+    listItem.textContent = item;
+
+    return listItem;
+  });
+};
+
+const elements = makeListItems(ingredients);
+ingredientsList.append(...elements);
+
+// console.log(elements);
