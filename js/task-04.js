97c71eed39d916a7c980fb095efb13143a300993
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-21 22:03:22 +0300

    add_task4

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueRefs = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', () => {
+  counterValue -= 1;
+  valueRefs.textContent = counterValue;
+});
+
+incrementBtn.addEventListener('click', () => {
+  counterValue += 1;
+  valueRefs.textContent = counterValue;
+});
