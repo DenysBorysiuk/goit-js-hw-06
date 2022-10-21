82a05940ce7f129f0d7ee0325abb1c80ab3d2d69
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-21 20:14:19 +0300

    refactoring_task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,16 @@
 const categories = document.querySelector('#categories');
-const itemsList = document.querySelectorAll('.item');
+const items = document.querySelectorAll('.item');
 
 console.log(`
 Number of categories: ${categories.children.length}
 
-Category: ${itemsList[0].firstElementChild.textContent}
-Elements: ${itemsList[0].lastElementChild.children.length}
+Category: ${items[0].firstElementChild.textContent}
+Elements: ${items[0].lastElementChild.children.length}
 
-Category: ${itemsList[1].firstElementChild.textContent}
-Elements: ${itemsList[1].lastElementChild.children.length}
+Category: ${items[1].firstElementChild.textContent}
+Elements: ${items[1].lastElementChild.children.length}
 
-Category: ${itemsList[2].firstElementChild.textContent}
-Elements: ${itemsList[2].lastElementChild.children.length}
+Category: ${items[2].firstElementChild.textContent}
+Elements: ${items[2].lastElementChild.children.length}
 
 `);
