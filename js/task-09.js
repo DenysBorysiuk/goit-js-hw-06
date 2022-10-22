04751e85e311d4f0be6e4421e9e847767c35cb56
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-23 01:10:24 +0300

    add_task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorValue = document.querySelector('.color');
+const btnChange = document.querySelector('.change-color');
+
+btnChange.addEventListener('click', onChangeCl);
+
+function onChangeCl() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = document.body.style.backgroundColor;
+}
