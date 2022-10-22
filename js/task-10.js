523dd872a0f6aa40201885d8dcdb5d8befa75bd5
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-23 01:27:46 +0300

    add_task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlsRefs = document.querySelector('#controls');
+const btnCreateRefs = document.querySelector('button[data-create]');
+const btnDestroyRefs = document.querySelector('button[data-destroy]');
+const boxesRefs = document.querySelector('#boxes');
+
+btnCreateRefs.addEventListener('click', onCreateBoxes);
+btnDestroyRefs.addEventListener('click', onDestroyBoxes);
+
+function onCreateBoxes(params) {
+  console.log('create');
+}
+
+function onDestroyBoxes(params) {
+  console.log('destroy');
+}
