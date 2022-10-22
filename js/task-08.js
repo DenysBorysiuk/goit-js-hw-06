282ff008780f3ca33836bb436c4988239e8f6642
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-23 00:24:40 +0300

    refactoring_task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,19 @@
 const form = document.querySelector('.login-form');
-form.addEventListener('submit', onFormSubmit);
+form.addEventListener('submit', onSubmit);
 
-function onFormSubmit(evt) {
+function onSubmit(event) {
   event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
 
-  console.log(form.elements.email.value);
-  console.log(form.elements.password);
-
-  if (!form.elements.email.value.length & !form.elements.password.value.length) {
-    alert('Все поля должны быть заполнены');
+  if (email.value === '' || password.value === '') {
+    return console.log('Все поля должны быть заполнены!');
   }
-}
 
-// Обработка отправки формы form.login-form должна быть по событию submit.
-// При отправке формы страница не должна перезагружаться.
-// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
-// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет
-// именем свойства, а значение поля - значением свойства.Для доступа к элементам формы используй свойство elements.
-// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+  const obj = {
+    [email.name]: email.value,
+    [password.name]: password.value,
+  };
+
+  console.log(obj);
+  event.currentTarget.reset();
+}
