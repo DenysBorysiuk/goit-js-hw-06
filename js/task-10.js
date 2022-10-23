812a56e49db793f63150924ebdfae3944cb37367
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-23 03:21:19 +0300

    process_task10

--- js/task-10.js
+++ js/task-10.js
@@ -7,13 +7,35 @@ const btnCreateRefs = document.querySelector('button[data-create]');
 const btnDestroyRefs = document.querySelector('button[data-destroy]');
 const boxesRefs = document.querySelector('#boxes');
 
-btnCreateRefs.addEventListener('click', onCreateBoxes);
+btnCreateRefs.addEventListener('click', onGetAmount);
 btnDestroyRefs.addEventListener('click', onDestroyBoxes);
 
-function onCreateBoxes(params) {
-  console.log('create');
+function onDestroyBoxes() {
+  boxesRefs.innerHTML = '';
+  controlsRefs.firstElementChild.value = '';
 }
 
-function onDestroyBoxes(params) {
-  console.log('destroy');
+function onGetAmount() {
+  createBoxes(controlsRefs.firstElementChild.value);
 }
+
+function createBoxes(amount) {
+  const arr = [];
+  for (let i = 0, l = 30; i < amount; i += 1, l += 10) {
+    const item = document.createElement('div');
+    item.style.backgroundColor = getRandomHexColor();
+    item.style.width = `${l}px`;
+    item.style.height = `${l}px`;
+    arr.push(item);
+  }
+  boxesRefs.append(...arr);
+}
+
+// Создай функцию createBoxes(amount), которая принимает один параметр - число.Функция создает столько < div >,
+//   сколько
+// указано в amount и добавляет их в div#boxes.
+
+// Размеры самого первого <div> - 30px на 30px.
+// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+// Все элементы должены иметь случайный цвет фона в формате HEX.
+// Используй готовую функцию getRandomHexColor для получения цвета.
